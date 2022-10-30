af7c23a69094a51f9a074ebb9ff3b1fa2711f084
Taha Sari <saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.com>
2022-10-31 00:30:25 +0300

    commit2-bugsfixed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void primeNumber(){
+        Scanner scanner=new Scanner(System.in);
+        int number;int divisor=0;
+        System.out.println("Please enter a number");
+        number= scanner.nextInt();
+        
+        for(int i=4;i<number;i++){
+             if(number%i==0){
+                 divisor++;
+             }
+        }if(divisor==0){System.out.println("Prime number");}
+        else System.out.println("isn't a prime number");
+
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        primeNumber();
     }
 }
